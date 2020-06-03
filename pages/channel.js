bb80045f46f0b148c2209a4055d7b5476e8be217
Razielini <razielco@gmail.com>
2020-06-03 13:05:25 -0600

    Update channel.js

--- pages/channel.js
+++ pages/channel.js
@@ -1,25 +1,95 @@
 import Layout from '../components/Layout'
 import ChannelGrid from '../components/ChannelGrid'
-import PodcastList from '../components/PodcastList'
+import PodcastListWithClick from '../components/PodcastListWithClick'
 import Error from './_error'
+import PodcastPlayer from '../components/PodcastPlayer'
 
-function Channel ({ channel, audioClips, channelSeries, statusCode }) {
-  if (statusCode !== 200) return <Error statusCode={ statusCode } />
+export default class Channel extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { openPodcast: null }
+  }
 
-  return (
-    <div>
-      <Layout title={channel.title}>
-        <h2>Últimos Podcasts</h2>
-        <PodcastList audio_clips={ audioClips } />
-        <h2>Series</h2>
-        <ChannelGrid channels={ channelSeries } />
-      </Layout>
-    </div>
-  )
+  openPodcast = (event, podcast) => {
+    event.preventDefault()
+    this.setState({
+      openPodcast: podcast
+    })
+  }
+
+  closePodcast = (event) => {
+    event.preventDefault()
+    this.setState({
+      openPodcast: null
+    })
+  }
+
+  render () {
+    const { channel, audioClips, channelSeries, statusCode } = this.props
+    const { openPodcast } = this.state
+
+    if (statusCode !== 200) return <Error statusCode={ statusCode } />
+
+    return (
+      <div>
+        <Layout title={channel.title}>
+          <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
+
+          {
+            openPodcast && 
+            <div className="modal">
+              <PodcastPlayer clip={ openPodcast } onClose={ this.closePodcast } />
+            </div>
+          }
+      
+          <h1>{ channel.title }</h1>
+          <h2>Últimos Podcasts</h2>
+          <PodcastListWithClick podcasts={ audioClips } onClickPodcast={ this.openPodcast } />
+
+          { channelSeries.length > 0 &&
+            <div>
+              <h2>Series</h2>
+              <ChannelGrid channels={ channelSeries } />
+            </div>
+          }
+
+          <style jsx>{`
+            .banner {
+              width: 100%;
+              padding-bottom: 25%;
+              background-position: 50% 50%;
+              background-size: cover;
+              background-color: #aaa;
+            }
+            h1 {
+              font-weight: 600;
+              padding: 15px;
+            }
+            h2 {
+              padding: 15px;
+              font-size: 1.2em;
+              font-weight: 600;
+              margin: 0;
+            }
+
+            .modal {
+              position: fixed;
+              top: 0;
+              left: 0;
+              right: 0;
+              bottom: 0;
+              background: #000;
+              z-index: 99999;
+            }
+          `}</style>
+        </Layout>
+      </div>
+    )
+  }
 }
 
-export const getServerSideProps = async ({ query, res }) => {
-  console.log('getServerSideProps ::', query)
+export async function getServerSideProps({ query, res }) {
+  // console.log('getServerSideProps ::', query)
   try {
     let idChannel = query.id
 
@@ -43,11 +113,9 @@ export const getServerSideProps = async ({ query, res }) => {
     let dataSeries = await reqSeries.json()
     let channelSeries = dataSeries.body.channels
 
-    console.log('getInitialProps ::', channel)
+    // console.log('getInitialProps ::', channel)
     return { props: { channel, audioClips, channelSeries, statusCode: 200 } }
   } catch (error) {
     return { props: { channel: null, audioClips: null, channelSeries: null, statusCode: 503 } }
   }
 }
-
-export default Channel
